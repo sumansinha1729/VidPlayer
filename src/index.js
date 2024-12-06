4824455cc4e100bf0660c00bbaacbb90e468ca89
sumansinha1729 <sumansinha437@gmail.com>
2024-12-06 21:12:13 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path:'./env'})
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
@@ -16,6 +17,8 @@ connectDB()
 .catch((err)=>{
     console.log("MONGO db connection failed !!! ",err);
 })
+
+app
 
 
 
